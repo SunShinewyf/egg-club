2d55db7bdbac5dbf1f2cb147a7a2da1f46813761
SunShinewyf <2632807692@qq.com>
2017-09-23 18:21:24 +0800

    add the unittest of find function in service/user.js/

--- test/app/controller/home.test.js
+++ test/app/controller/home.test.js
@@ -1,6 +1,30 @@
+// 'use strict';
+
+// const { app, assert } = require('egg-mock/bootstrap');
+
+// describe('test/app/controller/home.test.js', () => {
+
+//   it('should assert', function* () {
+//     const pkg = require('../../../package.json');
+//     assert(app.config.keys.startsWith(pkg.name));
+
+//     // const ctx = app.mockContext({});
+//     // yield ctx.service.xx();
+//   });
+
+//   it('should GET /', () => {
+//     return app.httpRequest()
+//       .get('/')
+//       .expect('hi, egg')
+//       .expect(200);
+//   });
+// });
+
+
 'use strict';
 
 const { app, assert } = require('egg-mock/bootstrap');
+// const assert = require('power-assert');
 
 describe('test/app/controller/home.test.js', () => {
 
@@ -11,11 +35,10 @@ describe('test/app/controller/home.test.js', () => {
     // const ctx = app.mockContext({});
     // yield ctx.service.xx();
   });
-
-  it('should GET /', () => {
-    return app.httpRequest()
-      .get('/')
-      .expect('hi, egg')
-      .expect(200);
+  // const arr = [ 1, 2, 3 ];
+  it('power-assert', async function() {
+    const ctx = app.mockContext({});
+    const users = await ctx.service.user.find('2632807692@qq.com');
+    assert.equal(users.length, 2);
   });
 });
